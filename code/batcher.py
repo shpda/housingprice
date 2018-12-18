fixed a bug of dev loss calculation, finished final report

code/batcher.py
@@ -142,6 +142,7 @@ class HousingPriceData(Dataset):
     def __getitem__(self, index):
         """ Get a sample from the dataset
         """
+        imageName = None
         if self.images is not None:
             # If dataset is preloaded
             image = self.images[index]
@@ -150,6 +151,7 @@ class HousingPriceData(Dataset):
         else:
             # If on-demand data loading
             image_fn = self.filenames[index]
+            imageName = image_fn
             image = Image.open(image_fn)
             if self.labels:
                 label = float(self.labels[index])
@@ -163,8 +165,10 @@ class HousingPriceData(Dataset):
         in_sentence = torch.tensor(sentence, dtype=torch.long)
         in_feature = torch.tensor(feature, dtype=torch.float)
         llabel = np.log(label + 1)
+        #in_llabel = torch.tensor(llabel, dtype=torch.float)
         # return image, sentence and label
-        return (image, in_sentence, in_feature), llabel
+        #return (image, in_sentence, in_feature), in_llabel
+        return (image, in_sentence, in_feature), llabel, imageName
 
     def __len__(self):
         """

code/hp_model.py
@@ -15,10 +15,8 @@ def getModel(mode, device, input_size, hidden_dim, emb_matrix):
     print("Start building model ...")
     tic = time.time()
     model = HousingPriceModel(input_size, hidden_dim, emb_matrix)
-    '''
     if device != None:
         model = model.cuda(device)
-    '''
     toc = time.time()
     print("Build model took %.2f s" % (toc-tic))
 
@@ -33,11 +31,11 @@ class HousingPriceModel(nn.Module):
         #self.resnet = torchvision.models.resnet101(pretrained=True)
         self.resnet = torchvision.models.resnet50(pretrained=True)
         self.resnetFeatures = nn.Sequential(*list(self.resnet.children())[:-4]) # conv3_x
-	self.mac = nn.MaxPool2d(input_size // 8, stride=1)
+        self.mac = nn.MaxPool2d(input_size // 8, stride=1)
 
         # for text feature extraction
         vocab_size = len(emb_matrix)
-	self.embedding_dim = len(emb_matrix[0])
+        self.embedding_dim = len(emb_matrix[0])
         self.hidden_dim = hidden_dim
         self.word_embeddings = nn.Embedding(vocab_size, self.embedding_dim)
         self.word_embeddings.weight.data.copy_(torch.from_numpy(emb_matrix))
@@ -46,6 +44,7 @@ class HousingPriceModel(nn.Module):
         
         # linear layer
         # 512 + 50 + 7 = 569
+        #self.classifier = nn.Linear(512, 1)
         self.classifier = nn.Linear(569, 1)
 
         #for p in self.features.parameters():
@@ -53,20 +52,20 @@ class HousingPriceModel(nn.Module):
 
 
     def forward(self, x):
-	image, sentence, feature = x
+        image, sentence, feature = x
 
         # for image feature extraction
         #print('image: ' + str(image.shape))
         resnet_out = self.resnetFeatures(image)
         #print('resnet_out: ' + str(resnet_out.shape))
-	mac_resnet_out = self.mac(resnet_out)
+        mac_resnet_out = self.mac(resnet_out)
         #print('mac_resnet_out: ' + str(mac_resnet_out.shape))
-	squeezed_mac_resnet_out = torch.squeeze(mac_resnet_out) 
+        squeezed_mac_resnet_out = torch.squeeze(mac_resnet_out) 
         #print('squeezed_mac_resnet_out: ' + str(squeezed_mac_resnet_out.shape))
 
         # for text feature extraction
         #print('sentence: ' + str(sentence.shape))
-	embeds = self.word_embeddings(sentence)
+        embeds = self.word_embeddings(sentence)
         #print('embeds: ' + str(embeds.shape))
         #print('embeds: ' + str(embeds.view(sentence.shape[1], self.batch_size, -1).shape))
         #print('hidden0: ' + str(self.hidden[0].shape))
@@ -76,7 +75,7 @@ class HousingPriceModel(nn.Module):
         mean_lstm_out = torch.mean(lstm_out, 0)
         #print('mean_lstm_out: ' + str(mean_lstm_out.shape))
 
-	fv = torch.cat((squeezed_mac_resnet_out, mean_lstm_out, feature), 1)
+        fv = torch.cat((squeezed_mac_resnet_out, mean_lstm_out, feature), 1)
         #print('fv: ' + str(fv.shape))
         #fv = fv.view(fv.size(0), -1)
         fv = self.classifier(fv)

code/trainer.py
@@ -15,7 +15,7 @@ from sklearn.decomposition import IncrementalPCA
 
 class Trainer():
     def __init__(self, mode, model, loader, dev_loader, optimizer, device, exp_path, 
-                 log_interval=5, eval_interval=5, save_interval=100):
+                 log_interval=1, eval_interval=5, save_interval=100):
         self.model = model
         self.loader = loader
         self.dev_loader = dev_loader
@@ -41,20 +41,21 @@ class Trainer():
             self.dev_logger = Logger(exp_path, 'dev', 'w')
 
     def train(self, epoch=5):
-        self.model.train()  # set training mode
         self.iteration = 0 
         best_dev_loss = self.eval()
 
         for ep in range(epoch):
             epoch_tic = time.time()
-            for batch_idx, (data, target) in enumerate(self.loader):
-                '''
-                if self.device != None:
-                    data, target = data.cuda(self.device), target.cuda(self.device)
-                '''
+            for batch_idx, (data, target, imageName) in enumerate(self.loader):
+                self.model.train()  # set training mode
                 self.optimizer.zero_grad()
                 #self.model.zero_grad()
                 self.model.hidden = self.model.init_hidden(data[0].shape[0])
+
+                if self.device != None:
+                    data = (data[0].cuda(self.device), data[1].cuda(self.device), data[2].cuda(self.device))
+                    target = target.cuda(self.device)
+                    self.model.hidden = (self.model.hidden[0].cuda(self.device), self.model.hidden[1].cuda(self.device))
 
                 # forward pass
                 output = self.model(data)
@@ -95,22 +96,63 @@ class Trainer():
             name = 'dev'
         self.model.eval()  # set evaluation mode
         loss = 0
-        correct = 0
+        epoch = 0
         with torch.no_grad():
-            for data, target in loader:
-                '''
-                if self.device != None:
-                    data, target = data.to(self.device), target.to(self.device)
-                '''
+            maxDiff = 0
+            worstImage = None
+            realPrice = None
+            predictedPrice = None
 
+            minDiff = sys.maxsize
+            bestImage = None
+            brealPrice = None
+            bpredictedPrice = None
+            for data, target, imageName in loader:
                 # calculate accumulated loss
                 self.model.hidden = self.model.init_hidden(data[0].shape[0])
 
-                output = self.model(data)
-                loss += self.loss_fn(torch.squeeze(output), target.float()) # sum up batch loss
+                if self.device != None:
+                    data = (data[0].cuda(self.device), data[1].cuda(self.device), data[2].cuda(self.device))
+                    target = target.cuda(self.device)
+                    self.model.hidden = (self.model.hidden[0].cuda(self.device), self.model.hidden[1].cuda(self.device))
+                    #data, target = data.to(self.device), target.to(self.device)
 
-        loss /= len(loader.dataset)
+                output = self.model(data)
+                curLoss = self.loss_fn(torch.squeeze(output), target.float())
+
+                # for error analysis
+                '''
+                for out, tgt, image in zip(torch.squeeze(output), target.float(), imageName):
+                    #print((abs(out.item() - tgt.item()), maxDiff, image))
+                    if abs(out.item() - tgt.item()) > maxDiff:
+                        maxDiff = abs(out.item() - tgt.item())
+                        predictedPrice = np.exp(out.item()) - 1
+                        realPrice = np.exp(tgt.item()) - 1
+                        worstImage = image
+
+                    if abs(out.item() - tgt.item()) < minDiff:
+                        minDiff = abs(out.item() - tgt.item())
+                        bpredictedPrice = np.exp(out.item()) - 1
+                        brealPrice = np.exp(tgt.item()) - 1
+                        bestImage = image
+                '''
+
+                #print(curLoss.item())
+                loss += curLoss
+                epoch += 1
+                #print(loss.item())
+
+        loss /= epoch
+        # for error analysis
+        '''
+        print('worstImage: %s' % worstImage)
+        print('predicted price: %s' % predictedPrice)
+        print('real price: %s' % realPrice)
+        print('bestImage: %s' % bestImage)
+        print('predicted price2: %s' % bpredictedPrice)
+        print('real price2: %s' % brealPrice)
         print('{} set: Average loss: {:.4f}'.format(name, loss.item()))
+        '''
         if name == 'dev':
             self.dev_logger.writeLoss(self.iteration, loss.item())
 

code/utils.py
@@ -72,7 +72,7 @@ def saveModel(checkpoint_path, model, optimizer):
     state = {'state_dict': model.state_dict(),
              'optimizer' : optimizer.state_dict()}
     torch.save(state, checkpoint_path)
-    print('model saved to %s' % checkpoint_path)
+    #print('model saved to %s' % checkpoint_path)
 
 def loadModel(checkpoint_path, model, optimizer):
     state = torch.load(checkpoint_path)